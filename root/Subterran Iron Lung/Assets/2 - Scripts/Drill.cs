46ea56e36959b698004f292e1b329c0321913db6
Spdearing <142638816+Spdearing@users.noreply.github.com>
2023-08-21 23:09:59 -0400

    Created a Drill script that can be applied to different interactable

---
+++ root/Subterran Iron Lung/Assets/2 - Scripts/Drill.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Drill : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        CheckForInteractable();
+    }
+
+    private void CheckForInteractable()
+    {
+        // Cast a ray from the drill's tip forward
+        Ray ray = new Ray(drill.position, drill.forward);
+        RaycastHit hit;
+
+        if (Physics.Raycast(ray, out hit))
+        {
+            // Check if the hit object is the underwater terrain
+            if (hit.collider.CompareTag("IInteractable"))
+            {
+                // Perform drilling effects on the terrain
+                // You can adjust terrain height, textures, and more here
+            }
+        }
+    }
+}
+
--- root/Subterran Iron Lung/Assets/2 - Scripts/ShipManager.cs
+++ root/Subterran Iron Lung/Assets/2 - Scripts/ShipManager.cs
@@ -2,8 +2,9 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ShipManager : MonoBehaviour
+public class ShipManager : MonoBehaviour , IInteractable
 {
+    
 
 
     // Start is called before the first frame update
@@ -16,5 +17,16 @@ public class ShipManager : MonoBehaviour
     void Update()
     {
         
+    }
+
+    void IInteractable.Interact(ShipManager manager)
+    {
+
+    }
+
+    void IInteractable.Interact(ManagerToObjectivePacket dataPacket, ShipManager manager)
+    {
+        //datapacket tells the object to update certain things like its mesh
+
     }
 }
